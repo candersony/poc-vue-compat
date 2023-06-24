12dede3ab8926cf608623fcf3b7f64090669b04e
Craig Anderson <craig.anderson@ankorstore.com>
2023-06-24 23:17:09 +0100

    feat(component-library): Add vue-demi

---
+++ packages/component-library/lib/App.tsx
@@ -0,0 +1,17 @@
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({
+  name: "App",
+  render() {
+    return (
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+    );
+  },
+});
